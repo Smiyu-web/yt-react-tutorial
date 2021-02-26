fcb7ffe5a9e5eda16e36285c5cabf361ad614837
Miu <miu@Mius-MacBook-Pro.local>
2021-02-26 15:00:57 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { List } from './List';
 
 function App() {
+  const [description, setDescription] = useState('before');
+
+  const changeDescription = () => {
+    setDescription('after')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { description }
+      <List title="property list" />
+      <button onClick={changeDescription}>Button</button>
     </div>
   );
 }
