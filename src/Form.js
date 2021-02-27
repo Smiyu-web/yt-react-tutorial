4d0f2d28e37ea090de3b9e8296a96ff9580479a9
Miu <miu@Mius-MacBook-Pro.local>
2021-02-26 16:44:07 -0800

    added form

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,31 @@
-export const Form = () => {
+import { useState } from 'react';
+
+
+export const Form = ({ onAddLang }) => {
+  const [text, setText] = useState('')
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    onAddLang(text)
+  }
+
   return (
-    <div>Form</div>
+    <div>
+      <h4>Ddd new propety</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input 
+            type="text" 
+            values={text} 
+            onChange={(e) => 
+              setText(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+    </div>
   )
 }
